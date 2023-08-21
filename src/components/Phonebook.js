a3437999a2f191292650251ed98d412ae038a59f
Marko Alhamadani <Markoalhamadani@gmail.com>
2023-08-21 09:38:49 -0400

    fixed delete

--- src/components/Phonebook.js
+++ src/components/Phonebook.js
@@ -1,16 +1,13 @@
-const Phonebook = ({persons, filterName, removeEntry}) => {
-    let newPersonArray = persons.filter((person) => 
-    person.name.toLowerCase().includes(filterName.toLowerCase()))
+const Phonebook = ({ persons, filterName, removeEntry }) => {
+	let newPersonArray = persons.filter((person) =>
+		person.name.toLowerCase().includes(filterName.toLowerCase())
+	);
 
-    return(
-        newPersonArray.map(person => (
-            <li key={person.id}>
-                {person.name} : {person.number}
-                <button onClick={()=> removeEntry(person.id)}>
-                    Delete
-                </button>
-            </li>
-        ))
-    )
-}
-export default Phonebook
+	return newPersonArray.map((person) => (
+		<li key={person.id}>
+			{person.name} : {person.number} : {person.id}
+			<button onClick={() => removeEntry(person.id)}>Delete</button>
+		</li>
+	));
+};
+export default Phonebook;
